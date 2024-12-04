9f0419d9435b7f0db0953988580cbff9cc332200
likebocai <likebocai@qq.com>
2024-12-04 16:33:53 +0800

    feature: RedisTemplate<String, Object> kv序列化

---
+++ common/common-config/src/main/java/com/likebocai/share/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package com.likebocai.share.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Resource;
+import org.redisson.client.RedisClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @program: likebocai-java
+ * @description: redis配置类
+ * @author: LikeBocai
+ * @create: 2024/12/4 15:59
+ **/
+@Configuration
+public class RedisConfig {
+
+    /**
+     * 配置 RedisTemplate，设置键值的序列化方式
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        // 设置键的序列化器
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        template.setKeySerializer(stringRedisSerializer);
+        template.setHashKeySerializer(stringRedisSerializer);
+
+        // 设置值的序列化器
+        GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
