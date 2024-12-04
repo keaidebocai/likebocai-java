c0e69a3824f78c6f7b9d85d6fbcf996ec165bb90
likebocai <likebocai@qq.com>
2024-12-04 14:24:14 +0800

    feature: 添加 share 模块

---
+++ server/server-share/src/main/java/com/likebocai/share/ShareApplication16287.java
@@ -0,0 +1,11 @@
+package com.likebocai.share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.likebocai.share"})
+public class ShareApplication16287 {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareApplication16287.class, args);
+    }
+}
