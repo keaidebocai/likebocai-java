bfcfe1654d842309b3d484a746cd90d3e7b19fcf
likebocai <likebocai@qq.com>
2024-12-04 16:34:10 +0800

    feature: 添加 oauth 模块

---
+++ server/server-oauth/src/main/java/com/likebocai/oauth/OauthApplication16281.java
@@ -0,0 +1,11 @@
+package com.likebocai.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"com.likebocai.oauth"})
+public class OauthApplication16281 {
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication16281.class, args);
+    }
+}
