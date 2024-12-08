3706ec2910f2ee94db01a876ab3faf804fc8db12
likebocai <likebocai@qq.com>
2024-12-08 18:51:56 +0800

    feature: validation-demo-加密密文长度校验

--- common/common-util/src/main/java/com/likebocai/common/validation/DecryptLengthValidation.java
+++ common/common-util/src/main/java/com/likebocai/common/validation/DecryptLengthValidation.java
@@ -1,8 +1,14 @@
 package com.likebocai.common.validation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likebocai.common.context.ValidationContext;
 import com.likebocai.common.validation.constraints.DecryptRuleByLength;
+import jakarta.annotation.Resource;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
 
 /**
  * @program: likebocai-java
@@ -10,7 +16,7 @@ import jakarta.validation.ConstraintValidatorContext;
  * @author: LikeBocai
  * @create: 2024/12/7 19:35
  **/
-
+@Slf4j
 public class DecryptLengthValidation implements ConstraintValidator<DecryptRuleByLength,String> {
 
     private int minLength;
@@ -18,6 +24,9 @@ public class DecryptLengthValidation implements ConstraintValidator<DecryptRuleB
     private int maxLength;
 
     private String fileName;
+
+    @Resource
+    private ObjectMapper objectMapper;
 
     @Override
     public void initialize(DecryptRuleByLength constraintAnnotation) {
@@ -28,8 +37,18 @@ public class DecryptLengthValidation implements ConstraintValidator<DecryptRuleB
 
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        // TODO: 这里写将加密内容还原后，判断原内容的长短
-        // TODO: 难点为 怎么获取到该类的时间戳来进行解密
+        // TODO: 1.这里写将加密内容还原后，判断原内容的长短
+        // TODO: 2.难点为 怎么获取到该类的时间戳来进行解密
+        Object requestBody = ValidationContext.getRequestBody();
+        Map<String,Object> fileNameMap = objectMapper.convertValue(requestBody,Map.class);
+        String fileValue = fileNameMap.get(fileName).toString();
+        log.info("DecryptLengthValidation -> isValid -> 拿requestBody: {}", fileNameMap);
+        log.info("fileName -> {}", fileName);
+        log.info("fileName's value -> {}", fileValue);
+        boolean contains = fileValue.contains("3");
+        if (!contains) {
+            return false;
+        }
         if (value == null || value.length() < minLength || value.length() > maxLength) {
             return false;
         }
